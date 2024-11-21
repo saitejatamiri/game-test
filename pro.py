6b553ced906f01ff8065333374682d127e84625c
saitejatamiri <98472448+saitejatamiri@users.noreply.github.com>
2024-11-21 16:43:12 +0530

    Update pro.py

--- pro.py
+++ pro.py
@@ -3,13 +3,13 @@ import requests
 url = "https://app.launchdarkly.com/api/v2/projects"
 
 payload = {
-  "key": "KEY",
-  "name": "PRO"
+  "key": "123456789",
+  "name": "candy"
 }
 
 headers = {
   "Content-Type": "application/json",
-  "Authorization": "TOCK"
+  "Authorization": "api-205f67ed-bc69-45c2-b984-4e179818b659"
 }
 
 response = requests.post(url, json=payload, headers=headers)
